60d833cca0d97b94be608a711dfb792645345c0c
Dzhabrail Elmezov <elmezov007@gmail.com>
2020-03-06 06:48:37 +0300

    Update TeleBotKSM.py

--- codeBot/TeleBotKSM.py
+++ codeBot/TeleBotKSM.py
@@ -1,10 +1,52 @@
 import telebot
 import constants
 import saveLogToFile
-
+from telebot import types
 
 bot = telebot.TeleBot(constants.token)
 
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard1.row('Покупка', 'Продажа')
+
+keyboard2 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard2.row('Рис', 'Соя', 'Горох', 'Ячмень')
+saved_string = ""
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Добро пожаловать {0.first_name}! вас приветствует {1.first_name}. Я помогу выставить ваше объявление просто следйуте инструкциям: ".format(message.from_user, bot.get_me()))
+    bot.send_message(message.chat.id, "1) Покупаем или продаём(Выберите кнопку)", reply_markup=keyboard1)
+
+@bot.message_handler(content_types=['text'])
+def choose_sell_buy(message):
+    global saved_string
+    if message.text == 'Покупка':
+        bot.register_next_step_handler(bot.send_message(message.chat.id, "2) Выберите продукт для покупки: ", reply_markup=keyboard2), product_descriptions)
+        saved_string += message.text
+    elif message.text == 'Продажа':
+        bot.register_next_step_handler(bot.send_message(message.chat.id, "2) Выберите продукт для продажи: ", reply_markup=keyboard2), product_descriptions)
+        saved_string += '\n' + message.text
+
+def product_descriptions(message):
+    global saved_string
+    saved_string += '\n' + message.text
+    bot.register_next_step_handler(bot.send_message(message.chat.id, "3) Укажите основные характеристики товара(Цена/Вес/Регион)"), get_contact)
+
+def get_contact(message):
+    global saved_string
+    saved_string += '\n' + message.text
+    bot.register_next_step_handler(bot.send_message(message.chat.id, "4) Укажите контактные данные:"), end_poll)
+
+def end_poll(message):
+    global saved_string
+    saved_string += '\n' + message.text
+    bot.send_message(message.chat.id, "Спасибо за терпение, проверьте всё ли верно:\n " + saved_string)
+    print(saved_string)
+    saved_string = ""
+
+
+
+'''
 def log_new_message(message):
     print("\n ------")
     from datetime import datetime
@@ -25,8 +67,7 @@ def send_text(message):
     if message.text:
         bot.send_message(constants.chat_ksm_test_004, message.text)
         log_new_message(message)
-        
-
+        '''
 
 
 
