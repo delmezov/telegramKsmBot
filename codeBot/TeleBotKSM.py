035607e444258366f682c59f09cda4fc66a18e96
Dzhabrail Elmezov <elmezov007@gmail.com>
2020-03-06 20:32:03 +0300

    Update TeleBotKSM.py

--- codeBot/TeleBotKSM.py
+++ codeBot/TeleBotKSM.py
@@ -1,36 +1,41 @@
 import telebot
-import constants
+import config
 import saveLogToFile
 from telebot import types
 
-bot = telebot.TeleBot(constants.token)
+bot = telebot.TeleBot(config.token)
 
-keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('Покупка', 'Продажа')
+buy_sell_keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
+buy_sell_keyboard.row('Покупка', 'Продажа')
 
-keyboard2 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard2.row('Рис', 'Соя', 'Горох', 'Ячмень')
+seed_keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
+seed_keyboard.row('Рис', 'Соя', 'Горох', 'Ячмень')
 saved_string = ""
+
+choose_end_keybord = telebot.types.ReplyKeyboardMarkup(True, True)
+choose_end_keybord.row('Всё верно!', 'Заполнить заново')
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, "Добро пожаловать {0.first_name}! вас приветствует {1.first_name}. Я помогу выставить ваше объявление просто следйуте инструкциям: ".format(message.from_user, bot.get_me()))
-    bot.send_message(message.chat.id, "1) Покупаем или продаём(Выберите кнопку)", reply_markup=keyboard1)
+    bot.send_message(message.chat.id, "1) Покупаем или продаём(Выберите кнопку)", reply_markup=buy_sell_keyboard)
 
 @bot.message_handler(content_types=['text'])
 def choose_sell_buy(message):
     global saved_string
     if message.text == 'Покупка':
-        bot.register_next_step_handler(bot.send_message(message.chat.id, "2) Выберите продукт для покупки: ", reply_markup=keyboard2), product_descriptions)
+        bot.register_next_step_handler(bot.send_message(message.chat.id, "2) Выберите продукт для покупки: ", reply_markup=seed_keyboard), product_descriptions)
         saved_string += message.text
     elif message.text == 'Продажа':
-        bot.register_next_step_handler(bot.send_message(message.chat.id, "2) Выберите продукт для продажи: ", reply_markup=keyboard2), product_descriptions)
+        bot.register_next_step_handler(bot.send_message(message.chat.id, "2) Выберите продукт для продажи: ", reply_markup=seed_keyboard), product_descriptions)
         saved_string += '\n' + message.text
 
 def product_descriptions(message):
     global saved_string
     saved_string += '\n' + message.text
-    bot.register_next_step_handler(bot.send_message(message.chat.id, "3) Укажите основные характеристики товара(Цена/Вес/Регион)"), get_contact)
+    bot.register_next_step_handler(bot.send_message(message.chat.id, "3) Укажите основные характеристики товара(Цена/Вес/Регион)", reply_markup = types.ReplyKeyboardRemove()), get_contact)
+    
 
 def get_contact(message):
     global saved_string
@@ -40,9 +45,19 @@ def get_contact(message):
 def end_poll(message):
     global saved_string
     saved_string += '\n' + message.text
-    bot.send_message(message.chat.id, "Спасибо за терпение, проверьте всё ли верно:\n " + saved_string)
+    bot.register_next_step_handler(bot.send_message(message.chat.id, "Спасибо за терпение, проверьте всё ли верно:\n " + saved_string, reply_markup=choose_end_keybord), delete_keybord)
+    #bot.register_next_step_handler(bot.send_message((message.chat.id, "Спасибо за терпение, проверьте всё ли верно:\n " + saved_string), reply_markup = choose_end_keybord), delete_keybord)
+
     print(saved_string)
     saved_string = ""
+
+def delete_keybord(message):
+    bot.send_message(message.chat.id, "Спасибо ваше объявление отправлено", reply_markup = types.ReplyKeyboardRemove())
+
+
+
+                
+bot.polling(none_stop=True)
 
 
 
@@ -71,5 +86,3 @@ def send_text(message):
 
 
 
-
-bot.polling()
